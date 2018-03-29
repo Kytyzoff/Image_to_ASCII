ce33037bbf6651e14fa85005e90f08a112101a52
Kytyzoff <Kytyzoff92@gmail.com>
2018-03-29 04:27:36 +0300

    initial commit

--- img.py
+++ img.py
@@ -1,22 +1,28 @@
-from PIL import Image
+def img_to_ascii(img_path):
 
-image = Image.open("img.jpg")
-width = image.size[0]
-height = image.size[1]
-width_resized = width//3
-height_resized = height//3
-resized_image = image.resize((width_resized, height_resized), Image.ANTIALIAS)
-resized_image = resized_image.convert('L')
-pix = resized_image.load()
-f = open('image.txt','w')
-symbols = ['@','O','c','+','/','*','.',' ']
+    from PIL import Image
 
-for i in range(height_resized):
-    for j in range(width_resized):
-        a = pix[j, i]
-        n = 255 // int(len(symbols)-1)
-        f.write(symbols[(a//n)])
-    f.write('\n')
-f.close()
+    image = Image.open(img_path)
+    #image = Image.open("img.jpg")
+    width = image.size[0]
+    height = image.size[1]
+    width_resized = width//3
+    height_resized = height//3
+    resized_image = image.resize((width_resized, height_resized), Image.ANTIALIAS)
+    resized_image = resized_image.convert('L')
+    pix = resized_image.load()
+    f = open('image.txt','w')
+    symbols = ['@','O','c','+','/','*','.',' ']
 
+    for i in range(height_resized):
+        for j in range(width_resized):
+            a = pix[j, i]
+            n = 255 // int(len(symbols)-1)
+            f.write(symbols[(a//n)])
+        f.write('\n')
+    f.close()
 
+if __name__ == '__main__':
+    img_path = input()
+    print(img_path)
+    img_to_ascii(img_path)
